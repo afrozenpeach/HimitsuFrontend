fb7809cf8776369c9176c40af2ff97ff31ee2107
Mattie Schraeder <mattie@mattie.lgbt>
2021-08-26 13:47:11 -0500

    fix unit tests

--- src/app/npcs/npc/npc.component.spec.ts
+++ src/app/npcs/npc/npc.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
 
 import { NpcComponent } from './npc.component';
 
@@ -8,6 +10,7 @@ describe('NpcComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule, RouterModule.forRoot([], { relativeLinkResolution: 'legacy' })  ],
       declarations: [ NpcComponent ]
     })
     .compileComponents();
