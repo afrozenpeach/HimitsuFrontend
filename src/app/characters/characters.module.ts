30dff4595281711172215bb27a6790233c762c09
Mattie Schraeder <mattie@mattie.lgbt>
2021-08-27 00:12:16 -0500

    filterable characters page

--- src/app/characters/characters.module.ts
+++ src/app/characters/characters.module.ts
@@ -3,6 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ListComponent } from './list/list.component';
 import { CharacterComponent } from './character/character.component';
 import { RouterModule, Routes } from '@angular/router';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 const routes: Routes = [
   { path: '', component: ListComponent, pathMatch: 'full' },
@@ -16,6 +21,11 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule,
     RouterModule.forChild(routes)
   ]
 })
--- src/app/characters/list/list.component.ts
+++ src/app/characters/list/list.component.ts
@@ -1,24 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MysqlService } from '../../services/mysql.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent implements OnInit {
+export class ListComponent implements AfterViewInit {
 
-  characters: any[] = [];
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  characters = new MatTableDataSource<any>([]);
+  displayedColumns: string[] = ['icon', 'name', 'nicknames', 'journal', 'player', 'status', 'sect', 'hometown', 'house'];
 
   constructor(private mysql: MysqlService) { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     this.getCharacters();
   }
 
+  public doFilter = (event: Event) => {
+    this.characters.filter = (event.target as HTMLInputElement).value.trim().toLocaleLowerCase();
+  };
+
   private getCharacters() {
     this.mysql.getCharacters().then((response: any) => {
-      this.characters = response;
+      this.characters = new MatTableDataSource<any>(response);
+      this.characters.paginator = this.paginator;
+      this.characters.sort = this.sort;
     });
   }
 
